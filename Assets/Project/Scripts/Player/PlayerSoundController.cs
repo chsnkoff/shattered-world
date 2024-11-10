ab8649c7b52b352c34af35e2986e19c258bb4c89
Ilya <ilya.etogood@yandex.ru>
2024-11-10 17:58:10 +0300

    add: rock slurping

--- Assets/Project/Scripts/Player/PlayerSoundController.cs
+++ Assets/Project/Scripts/Player/PlayerSoundController.cs
@@ -1,15 +1,25 @@
+using Project.Scripts.Objects;
 using Project.Scripts.Player;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class PlayerStepSound : MonoBehaviour
+public class PlayerSoundController : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioClip[] _footstepSounds;
+    [SerializeField] private AudioClip _slurpSound;
     [SerializeField] private PlayerJumpController _jumpController;
     [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _stepInterval = 0.25f;
 
     private float _stepTimer;
+    private bool _isInRock;
+
+    private void OnEnable()
+    {
+        Rock.OnPlayerEntersRock.AddListener(OnPlayerEntersRock);
+        Rock.OnPlayerExitsRock.AddListener(OnPlayerExitsRock);
+    }
 
     private void Start()
     {
@@ -34,8 +44,23 @@ public class PlayerStepSound : MonoBehaviour
 
     private void PlayFootstepSound()
     {
-        if (_footstepSounds.Length <= 0) return;
+        if (_footstepSounds.Length <= 0 || _isInRock) return;
         var index = Random.Range(0, _footstepSounds.Length);
         _audioSource.PlayOneShot(_footstepSounds[index]);
+    }
+
+    private void OnPlayerEntersRock()
+    {
+        _isInRock = true;
+        _audioSource.clip = _slurpSound;
+        _audioSource.loop = true;
+        _audioSource.Play();
+    }
+    
+    private void OnPlayerExitsRock()
+    {
+        _audioSource.loop = false;
+        _audioSource.Stop();
+        _isInRock = false;
     }
 }
